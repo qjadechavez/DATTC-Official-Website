6c5ca82b0ba947bda84fef9d02086f48adf53b21
ram6000mhz <qesjyap@tip.edu.ph>
2025-10-22 22:15:16 +0800

    add missed file from commit

--- server/serverConfig/emailconfig.js
+++ server/serverConfig/emailconfig.js
@@ -1,6 +1,6 @@
-import nodemailer from "nodemailer";
+const nodemailer = require("nodemailer");
 
-export const transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
   secure: false,
@@ -9,3 +9,5 @@ export const transporter = nodemailer.createTransport({
     pass: process.env.EMAIL_PASS,
   },
 });
+
+module.exports = { transporter };
